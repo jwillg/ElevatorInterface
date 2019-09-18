3fac2da21c94fe350ae5910079a2374d1b8a4850
Josephine Willgrubs <47952922+jwillg@users.noreply.github.com>
2019-09-18 15:58:16 -0500

    Update buttons

--- buttons.js
+++ buttons.js
@@ -1,5 +1,35 @@
-const bellPlay = document.getElementById("myAudio");
 
-function playAudio() {
-    bellPlay.play();
+// functions for button select
+function bellLight(){
+    document.getElementById("bell").style.backgroundColor = "#ff7f7f";
 }
+
+function phoneLight(){
+    document.getElementById("phone").style.backgroundColor = "#7f7f7f";
+}
+
+function floor2Light(){
+    document.getElementById("floor2").style.backgroundColor = "#f5ff7f";
+
+}
+
+function floor1Light(){
+    document.getElementById("floor1").style.backgroundColor = "#f5ff7f";
+
+}
+
+function floorBLight(){
+    document.getElementById("floorB").style.backgroundColor = "#f5ff7f";
+
+}
+
+function doorOpenLight(){
+    document.getElementById("doorOpen").style.transition = "all 7s";
+
+}
+
+function doorCloseLight(){
+    document.getElementById("doorClose").style.transition = "all 7s";
+
+}
+
